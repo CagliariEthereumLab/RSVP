0aa85434ae9872632bc29036b8a12028e9ecc2a7
NormaN <normanargiolas@gmail.com>
2019-01-22 20:54:13 +0100

    add simple flow test

--- script/RSVP_test.js
+++ script/RSVP_test.js
@@ -20,6 +20,7 @@ const artifactKeccak256 = require('../build/Keccak256');
 
 let accounts;
 let account_admin;
+
 let rsvpAddress;
 let keccak256Address;
 
@@ -33,6 +34,8 @@ let stakes = [
 async function init() {
     accounts = await utils.getAccounts();
     // accounts = await web3.eth.getAccounts();
+    assert(accounts.length >= stakes.length, 'Per provare questo test assicurati di avere almeno 4 account');
+
     account_admin = accounts[0];
 
     console.log("accounts: " + accounts);
@@ -49,7 +52,7 @@ async function deploy() {
     rsvpAddress = result.address;
     console.log("RSVP address: " + rsvpAddress);
 
-    result = await RSVP.new();
+    result = await Keccak256.new();
     keccak256Address = result.address;
     console.log("Keccak256 address: " + keccak256Address);
 
@@ -57,22 +60,51 @@ async function deploy() {
 
 async function insert() {
 
+    let RSVP = await utils.getContractByAddress(artifactRSVP, account_admin, rsvpAddress);
+    let Keccak256 = await utils.getContractByAddress(artifactKeccak256, account_admin, keccak256Address);
+
+    for (let i = 0; i < stakes.length; i++) {
+        let k = await Keccak256.getKeccak256(stakes[i]);
+        let r = await RSVP.addCode(k);
+        console.log("Stake " + stakes[i] + " added with tx: " + r.tx);
+    }
+
 }
 
 async function verify() {
 
+    let RSVP = await utils.getContractByAddress(artifactRSVP, account_admin, rsvpAddress);
+
+    for (let i = 0; i < stakes.length; i++) {
+        let r = await RSVP.verifyCode.call(stakes[i]);
+        assert.equal(r, true);
+        console.log("Stake " + stakes[i] + " verifyed!");
+    }
 }
 
 async function reserve() {
 
+    let RSVP = await utils.getContractByAddress(artifactRSVP, account_admin, rsvpAddress);
+
+    for (let i = 1; i < stakes.length; i++) {
+        let r = await RSVP.reserveSeat({from: accounts[i+1], value: web3.utils.toWei('0.010', 'ether')});
+        console.log("Reserve stake for address: " + accounts[i] + " with tx " + r.tx);
+    }
 }
 
 async function stop() {
 
+    let RSVP = await utils.getContractByAddress(artifactRSVP, account_admin, rsvpAddress);
+    let r = await RSVP.stopRSVP();
+    console.log("Stop reservation with tx: " + r.tx);
 }
 
 async function unlock() {
-
+    let RSVP = await utils.getContractByAddress(artifactRSVP, account_admin, rsvpAddress);
+    for (let i = 1; i < stakes.length; i++) {
+        let r = await RSVP.unlockStake(stakes[i], {from: accounts[i+1]});
+        console.log("Unlock stake for address: " + accounts[i] + " with tx " + r.tx);
+    }
 }
 
 beforeEach(async () => {
@@ -92,25 +124,32 @@ describe('Test for blockchain start right', () => {
     });
 
     it("Insert stakes", async () => {
-        console.log("-----------------------------Deploy");
+        console.log("-----------------------------Insert");
         await insert();
     });
 
     it("Verify stakes", async () => {
+        console.log("-----------------------------Verify");
+
         await verify();
     });
 
     it("Reserve stakes", async () => {
+        console.log("-----------------------------Reserve");
+
         await reserve();
     });
 
     it("Stop reservation stakes", async () => {
+        console.log("-----------------------------Stop");
+
         await stop();
     });
 
     it("Unlock stakes", async () => {
+        console.log("-----------------------------Unlok");
+
         await unlock();
     });
-
 
 });
